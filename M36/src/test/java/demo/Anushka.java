9f6e11d533f08b60f7bd984b3818a71a4545118c
amruta <raskondaamruta32@gamil.com>
2025-09-26 07:30:06 +0530

    commit anushka

---
+++ M36/src/test/java/demo/Anushka.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Anushka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello anushka sharma");
+		System.out.println("hello anushka sharma");
+		System.out.println("hello anushka sharma");
+		System.out.println("hello anushka sharma");
+	}
+
+}
